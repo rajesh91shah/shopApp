4f59907e76bebdb918455b75d4f10551cbbb338b
Mac <mac@Macs-MacBook-Air.local>
2018-09-21 23:33:29 +0530

    first commit

---
+++ ShopManagment/ShopManagment/ViewController/CreateProductsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CreateProductsViewController.swift
+//  ShopManagment
+//
+//  Created by Mac on 21/09/18.
+//  Copyright © 2018 Mac. All rights reserved.
+//
+
+import UIKit
+
+class CreateProductsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
+
+extension CreateProductsViewController: UITableViewDelegate {
+
+}
+
+extension CreateProductsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.cellForRow(at: indexPath)
+        return cell!
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+}
